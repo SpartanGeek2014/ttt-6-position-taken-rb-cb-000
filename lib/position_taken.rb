d27e87e1d67492a93e4f8b6707862159af825bc7
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-10-05 02:11:02 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,13 +5,14 @@ def position_taken(board, index)
   taken = nil
   if (board[index] ==  " " || board[index] == "" || board[index] == nil)
     taken = false
-  else
+    elsif
     taken = true
-  end
-    taken
+      else
+        taken
 
   # advanced solution w/ ternary operator
-  # (board[index] ==  " " || board[index] == "" || board[index] == nil) ?
-  # false : true
-
+      (board[index] ==  " " || board[index] == "" || board[index] == nil) ?
+        false : true
+    end
+  end
 end
